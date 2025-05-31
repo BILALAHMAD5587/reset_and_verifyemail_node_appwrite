b26cd7066f4fcba159db3dc8226a81cc3802d8d2
BILALAHMAD5587 <114788522+BILALAHMAD5587@users.noreply.github.com>
2025-05-31 10:42:37 +0500

    Update auth_controllers.js

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -1,7 +1,7 @@
 import {Client,Account,ID,Functions} from "appwrite"
 
 const client = new Client()
-.setEndpoint('https://treasurenfts2.online/v1') // Your API Endpoint
+.setEndpoint('https://fra.cloud.appwrite.io/v1') // Your API Endpoint
 .setProject('67cc44870036518f1c7b')              // Your project ID
 
 const account = new Account(client);
