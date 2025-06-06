37979e8569b1d7f214fb177083fb9bc51e497cc2
BILALAHMAD5587 <114788522+BILALAHMAD5587@users.noreply.github.com>
2025-06-06 09:57:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -35,32 +35,6 @@ app.get('/nowpayments-ipn', (req, res) => {
   res.send('👍 This endpoint is ready to receive POST requests from NowPayments.');
 });
 
-// ----------- NowPayments IPN (actual logic) ------------
-app.post('/nowpayments-ipn', async (req, res) => {
-  try {
-    const ipnData = req.body;
-    console.log("📩 IPN Received:", ipnData);
-
-    // Appwrite Function Trigger
-    const response = await axios.post(
-      'https://fra.cloud.appwrite.io/v1/functions/68411df80026e2aec563/executions',
-      ipnData, // body if required
-      {
-        headers: {
-          'X-Appwrite-Project': '67cc44870036518f1c7b',
-          'X-Appwrite-Key': 'standard_9ef65c3845ee85eb727832014f9f315b7a58e3454dcff55f47f65460ead76eceeab30052f98331325b6bbec642a9d08778a72502c4b131f0a1571e54d45c84d64d731498734405981c4151a018d449c0465f68fd195d8a89ed3c5d078234f75b353b536037f2a80bb65b95625dbe5492f61b21700b8c9f4e8f372b7873bafe0f',
-          'Content-Type': 'application/json',
-        }
-      }
-    );
-
-    console.log("✅ Function Triggered:", response.data);
-    res.status(200).send('✔️ IPN receive ho gaya aur function trigger ho gaya!');
-  } catch (error) {
-    console.error("❌ IPN Error:", error?.response?.data || error.message);
-    res.status(500).send('❌ IPN process karte waqt error aya.');
-  }
-});
 
 // 🔸 Email verification endpoint
 app.get("/verify", async (req, res) => {
